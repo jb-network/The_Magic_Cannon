5dc2b8d9f77fce10e6246d8f5597adf202fdd9b8
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-23 13:00:14 -0500

    Add project files.

---
+++ The_Magic_Cannon/Program.cs
@@ -0,0 +1,22 @@
+﻿// This is challenge work for the "C# Players Guide"
+// Level 11 the Magic Cannon Challenge
+// Build a simple program to create a loop fires a cannon in the following manner:
+// Every 3rd crank of the cannon produces a fire bast
+// Every 5th crank of the cannon produces an electric blast
+// When fire and electric overlap, a combined blast is created
+// All other cranks provide a normal output
+// Firing should go from 1 to 100 times
+
+//Simple themed intro for challenge
+Console.WriteLine("Welcome to the cannon fire simulation program!");
+Console.WriteLine("This program will crank the cannon 100 times to see what is produced");
+Console.WriteLine("Press any key to begin simulation");
+Console.ReadKey();
+Console.Clear();
+Console.WriteLine("Firing 100 simulation rounds!");
+
+
+
+
+
+
